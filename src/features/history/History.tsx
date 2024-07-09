463d56fca8045b7a79eeed68d8fcdcaa908d4df6
Jackie Li <li.jackie565@gmail.com>
2024-07-09 19:46:28 -0400

    complete history feature

--- src/features/history/History.tsx
+++ src/features/history/History.tsx
@@ -1,18 +1,48 @@
+import "./styles.css";
+
 import { FC } from "react";
 import { useHistory } from "./api/useHistory";
+import { Button } from "../../components/button/Button";
+import { useUrlParams } from "../../hooks/useUrlParams";
+import { FormRule } from "../form/Form";
 
 export const History: FC = () => {
-  const { history, remove } = useHistory();
+  const { history, remove, clear } = useHistory();
+  const { setParam } = useUrlParams();
+
+  const handleSelect = (data: FormRule) => {
+    setParam("interval", String(data.interval));
+    setParam("rows", String(data.rows));
+    setParam("cols", String(data.cols));
+  };
+
+  const items = Object.entries(history);
+
   return (
-    <div>
-      {Object.entries(history).map(([id, value]) => (
-        <div key={id}>
-          <p>{value.interval}</p>
-          <p>{value.rows}</p>
-          <p>{value.cols}</p>
-          <button onClick={() => remove(id)}>Remove</button>
+    <div className="history-container">
+      <h2>Previous Simulations</h2>
+      {items.length ? (
+        <>
+          <Button onClick={() => clear()}>Clear</Button>
+          <div>
+            {items.map(([id, value]) => (
+              <div className="history" key={id}>
+                <p>Interval: {value.interval}</p>
+                <p>Rows: {value.rows}</p>
+                <p>Columns: {value.cols}</p>
+                <div className="button-container">
+                  <Button onClick={() => remove(id)}>Remove</Button>
+                  <Button onClick={() => handleSelect(value)}>Select</Button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <div>
+          <p>No previous simulations found.</p>
         </div>
-      ))}
+      )}
     </div>
   );
 };
